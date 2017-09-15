a41dd888e49da18a2d1c539ca27ad708e0affe1f
Jing Wei Li <ljw9801055@gmail.com>
2017-09-15 16:16:37 -0400

    Lab1-1 Li Version Finished

--- Lab1-1/lab1-1.c
+++ Lab1-1/lab1-1.c
@@ -1,13 +1,8 @@
-/*  Names: David _, David _, Jing Wei Li
+/*  Names: Jing Wei Li
     Section: 4
     Date: 9/15/17
     File name: lab1-1
     Program description:
-*/
-/*
-  This program is incomplete. Part of the code is provided as an example. You
-  need to modify the code, adding code to satisfy the stated requirements. Blank
-  lines have also been provided at some locations, indicating an incomplete line.
 */
 #include <c8051_SDCC.h> // include files. This file is available online
 #include <stdio.h>
@@ -16,17 +11,21 @@
 // Function Prototypes
 //-----------------------------------------------------------------------------
 void Port_Init(void);  // Initialize ports for input and output
-int sensor1(void);     // function which checks Pushbutton
-int sensor2(void);     // function that checks the Slide switch
+int isSlideSwitchOn(void); // function that checks the Slide switch
+int isPBOneOn(void);//function that checks PushButton 1
+int isPBTwoOn(void);// function that checks PushButton 2
 void Set_outputs(void);// function to set output bits
+void testInputs(void);
 
 //-----------------------------------------------------------------------------
-// Global Variables
+// Global Variables - outputs 1
 //-----------------------------------------------------------------------------
 __sbit __at 0xB6 LED0; // LED0, associated with Port 3 Pin 6
 __sbit __at 0xB3 BILED0; // BILED0, associated with P3.3
 __sbit __at 0xB4 BILED1; // BILED1, associated with P3.4
 __sbit __at 0xB7 BUZZER; // Buzzer, associated with P3.7
+
+//inputs 0
 __sbit __at 0xA0 SS;   // Slide switch, associated with Port 2 Pin 0
 __sbit __at 0xB0 PB1;  // Push button 1, associated with Port 3, Pin 0
 __sbit __at 0xB1 PB2; // Push button 2, P3.1
@@ -39,10 +38,8 @@ void main(void)
     putchar(' ');      // the quote fonts may not copy correctly into SiLabs IDE
     Port_Init();       // Initialize ports 2 and 3
 
-    while (1)          // infinite loop
-    {
+    while (1) {         // infinite loop
         // main program manages the function calls
-
         Set_outputs();
     }
 }
@@ -51,19 +48,15 @@ void main(void)
 //***************
 /* Port_Init - Initializes Ports 2 and 3 in the desired modes for input and output */
 
-void Port_Init(void)
-{
+void Port_Init(void) {
     // Port 3
     P3MDOUT &= 0xFC; // set Port 3 output pins to push-pull mode (fill in the blank)
-    P3MDOUT |= 0x98; // set Port 3 input pins to open drain mode (fill in the blank)
+    P3MDOUT |= 0xD8; // set Port 3 input pins to open drain mode (fill in the blank) // use to be 0x98
     P3 |= ~0xFC; // set Port 3 input pins to high impedance state (fill in the blank)
 
     // Port 2
-    // configure Port 2 as needed
     P2MDOUT &= 0xFE;
     P2 |= ~0xFE;
-//
-//
 }
 
 //***************
@@ -71,21 +64,53 @@ void Port_Init(void)
 //    The following code is incomplete, lighting an LED depending
 //    on the state of a single pushbutton.
 
-void Set_outputs(void)
-{
+/*
+ * Group Member 3
+1. When the Slide switch is ‘off’ (input is a HIGH voltage), all outputs are off
+2. When the Slide switch is ‘on’, the LED is on
+3. When the Slide switch is ‘on’ and both Pushbuttons are pushed, the Buzzer is turned on
+4. When the Slide switch is ‘on’ and only Pushbutton 1 is pushed, the BiLED is red
+5. When the Slide switch is ‘on’ and only Pushbutton 2 is pushed, the BiLED is green
+ */
 
-    if (sensor2())  // if Slide Switch activated
-    {
-        LED0 = 0;   // Light LED
-        printf("\rSlide switch is off \n");
+void Set_outputs(void) {
+	testInputs();
+
+    if (!isSlideSwitchOn()) { // slide switch is off
+        printf("\r All outputs are off \n");
+        LED0 = 1;
+        BILED0 = 0;
+        BILED1 = 0;
+        BUZZER = 1;
     }
 
-    else            // if Slide Switch is not activated
-    {
-        LED0 = 1;   // turn off LED
-        printf("\rSlide switch is on \n");
+    if (isSlideSwitchOn()){ // slide switch is on
+        printf("\r LED is On \n");
+        LED0 = 0;
+    } else { // turn LED off is slide switch is off
+        LED0 = 1;
     }
 
+    if (isSlideSwitchOn() && isPBOneOn() && isPBTwoOn()){
+        printf("\r Turning on Buzzer, both pushbutton pressed \n");
+        BUZZER = 0;
+    } else {
+        BUZZER = 1;
+    }
+
+    if (isSlideSwitchOn() && !isPBOneOn() && isPBTwoOn()){
+        printf("\r BILED is green, pushbutton 2 pressed\n");
+        BILED0 = 0;
+        BILED1 = 1;
+    } else if (isSlideSwitchOn() && isPBOneOn() && !isPBTwoOn()){
+        printf("\r BILED is red, pushbutton 1 pressed\n");
+        BILED0 = 1;
+        BILED1 = 0;
+    } else {
+        BILED0 = 0;
+        BILED1 = 0;
+
+    }
 }
 
 //***************
@@ -93,10 +118,13 @@ void Set_outputs(void)
 //          or a 1 if Pushbutton 1 is activated.
 //          This code reads a single input only, associated with PB0
 // Note this code is not used by function yet, you must incorporate it
-int sensor1(void)
-{
-
+int isPBOneOn(void) {
     if (!PB1) return 1;
+    else      return 0;
+}
+
+int isPBTwoOn(void) {
+    if (!PB2) return 1;
     else      return 0;
 }
 
@@ -104,10 +132,24 @@ int sensor1(void)
 // Sensor - Returns a 0 if Slide Switch is 'off'
 //          or a 1 if Slide switch  is 'on'
 //          This code reads a single input only, associated with SS
-int sensor2(void)
-{
-
+int isSlideSwitchOn(void) {
     if (!SS) return 1;
     else     return 0;
+}
+
+void testInputs(void){
+	if (isSlideSwitchOn()){
+		printf("\r SlideSwitch is ON \n");
+	}
+	if (isPBOneOn()){
+		printf("\r PUSHBUTTON 1 IS ON	\n");
+	} else {
+		printf("\r PushBUtton 1 is OFF");
+	}
+	if (isPBTwoOn()){
+		printf("\r PUSHBUTTON 2 IS ON	\n");
+	} else {
+		printf("\r PushBUtton 2 is OFF");
+	}
 }
 
