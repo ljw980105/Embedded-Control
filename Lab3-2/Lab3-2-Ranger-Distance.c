c592186fdad40ae6f62dd30a62160b1206e74d6f
Jing Wei Li <ljw9801055@gmail.com>
2017-10-30 21:59:29 -0400

    Finished Lab3-2 Ranger Distance

--- Lab3-2/Lab3-2-Ranger-Distance.c
+++ Lab3-2/Lab3-2-Ranger-Distance.c
@@ -1,5 +1,6 @@
 /* Lab3.2 - Ranger Distance*/
 #include <c8051_SDCC.h>
+#include <i2c.h>
 #include <stdio.h>
 #include <stdlib.h>
 //-----------------------------------------------------------------------------
@@ -9,6 +10,7 @@ void Port_Init(void);
 void PCA_Init (void);
 void XBR0_Init();
 void Set_Pulsewidth(void);
+void SMB_Init(void);
 void PCA_ISR ( void ) __interrupt 9;
 unsigned int ReadRanger();
 
@@ -21,8 +23,11 @@ unsigned int new_range = 0; // flag to determine whether a new range is ready to
 unsigned int r_count = 0;
 unsigned char addr = 0xE0; // the address of the ranger is 0xE0
 unsigned int ranger_distance;
+unsigned char Data[2];
+
 
 __sbit __at 0xDF CF;
+
 
 //-----------------------------------------------------------------------------
 // Main Function
@@ -39,14 +44,16 @@ void main(void)
     Port_Init();
     XBR0_Init();
     PCA_Init();
+    SMB_Init();
     while (1){
         if (new_range){ //if 80ms has passed
             ranger_distance = ReadRanger(); // read the range
             //start a ping
+			
             Data[0] = 0x51; // write 0x51 to reg 0 of the ranger:
             i2c_write_data(addr, 0, Data, 1); // write one byte of data to reg 0 at addr
             new_range = 0; //clear new range flag
-            printf("The current range is %d cm",ranger_distance);
+            printf("The current range is %d cm \r\n",ranger_distance);
         }
     }
 }
@@ -91,6 +98,12 @@ unsigned int ReadRanger() {
     range = (((unsigned int)Data[0] << 8) | Data[1]);
     return range;
 }
+
+void SMB_Init(void){
+    SMB0CR = 0x93; // set scl to 100khz
+    ENSMB = 1; // enable SMBus
+}
+
 
 //
 // Interrupt Service Routine for Programmable Counter Array Overflow Interrupt
