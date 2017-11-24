11ae43521a57142d701a11b767c80d5ce3839cd9
Jing Wei Li <ljw9801055@gmail.com>
2017-11-23 23:49:56 -0500

    Lab5 70% Done: Added Reverse Buzzer Pattern

--- Lab5/Lab5.c
+++ Lab5/Lab5.c
@@ -10,34 +10,37 @@ void PCA_Init (void);
 void XBR0_Init();
 void SMB_Init(void);
 void PCA_ISR ( void ) __interrupt 9;
-unsigned int ReadRanger();
 void PreventExtreme(void);
 unsigned char read_AD_input(unsigned char n);
 void ADC_Init(void);
-void preselectHeading(void);
+void preselectMotorSpd(void);
 
 void read_accel(void);
-
+void set_gains(void);
+void set_steering_pw(void);
+void set_driving_pw(void);
+void start_steering();
 
 //-----------------------------------------------------------------------------
 // Global Variables
 //-----------------------------------------------------------------------------
 unsigned char __xdata input, pw_percentage; // using __xdata to store large variables
-unsigned char CompassData[2], RangerData[2];
 unsigned char new_heading = 0;
 unsigned char new_range = 0;
 unsigned char print_flag = 0;
-unsigned char heading_count = 0;
-unsigned char ranger_count = 0;
+unsigned char accels_count = 0;
+unsigned char accels_flag = 0;
+unsigned char toggle_flag = 0;
+unsigned int reverse_count = 0;
 unsigned char LCD_count = 0;
-unsigned int desired_heading, heading,multipleInput, i, stops, ranger_distance,initial_heading;
+unsigned int multipleInput, i, stops;
 signed int error;
 int Kp_temp;
 float Kp;
 unsigned int counter_PCA = 0;
 unsigned int PW_Servo, PW_Motor, motor_spd;
 unsigned int PCA_START = 28614; //65535-36921
-unsigned int PW_CENTER = 2779; // PulseWidth is about 1.5ms 2769
+unsigned int PW_CENTER_MOTOR = 2779; // PulseWidth is about 1.5ms 2769
 unsigned int PW_CTR_SERVO = 2779;
 unsigned int PW_MIN = 2031; // 1.1ms full reverse
 unsigned int PW_MAX = 3508; // 1.9ms full forward
@@ -45,14 +48,18 @@ unsigned int PW_MAX = 3508; // 1.9ms full forward
 signed int avg_gx,avg_gy,gx,gy;
 unsigned char addr_accel = 0x3A;
 unsigned char AccelData[4];
+//respectively: steering gain, drive gain for x-dir & y-dir, intergal gain
+unsigned char ks,kdx,kdy,ki;
+unsigned char isReversed = 0;
+unsigned char battery_voltage;
 
 __sbit __at 0xB7 SS; // slide switch to enable/ disable servo and motor at P3.7
+__sbit __at 0xXX BUZZER; //TODO: Verify Buzzer Port
 
 //-----------------------------------------------------------------------------
 // Main Function
 //-----------------------------------------------------------------------------
-void main(void)
-{
+void main(void){
     // initialize board
     Sys_Init();
     putchar(' ');
@@ -64,45 +71,70 @@ void main(void)
     Accel_Init_C();
 
     printf("Embedded Control Pulsewidth Calibration\r\n");
-    PW_Motor = PW_CENTER;  // set pw to 1.5ms
+    PW_Motor = PW_CENTER_MOTOR;  // set pw to 1.5ms
     PW_Servo = PW_CTR_SERVO;
-    PCA0CP0 = 0xFFFF - PW_Motor;
-    PCA0CP2 = 0xFFFF - PW_Servo;
+    PCA0CP0 = 0xFFFF - PW_Servo;
+    PCA0CP2 = 0xFFFF - PW_Motor;
     counter_PCA = 0; //reset counter
     while(counter_PCA < 50);//wait for 1s for correct setup
 
     // read and ask the user to adjust the gains
     Kp = read_AD_input(7) / 25 ;  // read adc input at pin 1.7
+    stops = 0;
+    preselectMotorSpd();
 
     while(1) {
         if (!SS) { // if SS is on
-            // encounters the 1st obstacle: turn left/right based on input
+            if (stops == 0){
+                set_gains();
+                stops ++;
+            }
+            if (accels_flag){
+                read_accel();
+                set_steering_pw();
+                set_driving_pw();
+                start_driving();
+                start_steering();
+                accels_flag = 0;
+            }
 
-            PreventExtreme();
+            if (isReversed){
+                if(toggle_flag){
+                    BUZZER = 1;
+                    if(reverse_count % 25 == 0) toggle_flag = 0;
+                }
+                if (!toggle_flag){
+                    BUZZER = 0;
+                    if(reverse_count % 50 == 0) toggle_flag = 1;
+                }
+            }
+
         } else { //SS is not on: set everything to neutral
             PW_Servo = PW_CTR_SERVO; // Set Servo to neutral
-            PCA0CPL0 = 0xFFFF - PW_Servo;
-            PCA0CPH0 = (0xFFFF - PW_Servo) >> 8;
-            PW_Motor = PW_CENTER; // set motor to neutral
-            PCA0CPL2 = 0xFFFF - PW_Motor;
-            PCA0CPH2 = (0xFFFF - PW_Motor) >> 8;
-            PreventExtreme();
+            start_steering();
+            PW_Motor = PW_CENTER_MOTOR; // set motor to neutral
+            start_driving();
         }
+
+
         // print ranger distance and compass heading
-        pw_percentage = (abs(PW_Motor- PW_CENTER)*100)/(PW_MAX- PW_MIN);
         if (print_flag){ // update LCD
-			printf("%u %u %u\r\n",heading, ranger_distance, PW_Servo);
+            battery_voltage = read_AD_input(6);
+			printf("%d %d %d %d %d %d %d %d %d\r\n",gx,gy,kdx,kdy,ks,ki,PW_Motor,PW_Servo,battery_voltage);
 			lcd_clear();
-            lcd_print("direction: %u\nrange: %u\npw perc: %u\nbattery: %u\n",heading,ranger_distance,pw_percentage,read_AD_input(6));
+            lcd_print("ks: %u, kdx: %u, kdy: %u\nDrive: %u, Servo: %u\n, Battery: %u\n",ks,kdx,kdy,PW_Motor, PW_Servo,battery_voltage);
             print_flag = 0;
         }
     }
 }
 
+/*
+* Read 8 values from the accelerometer to average out the noise
+*/
 void read_accel(){
     avg_gy = avg_gx = gx = gy =0;
 
-    for (i = 0; i < 8; i++){ //8 iterations to reduce noise
+    for (i = 0; i < 8; i++){ //8 iterations
 
         i2c_read_data(addr_accel,0x27,AccelData,1);
         if (AccelData[0] & 0x03 == 0x03){ // accelerometer ready
@@ -141,100 +173,53 @@ void adjust_gain(){
     }
 }
 
+/*
+* Enter the ks,kdx,kdy,ki values using the keypad
+*/
+void set_gains(){
+    lcd_clear();
+	lcd_print("Enter value for ks\r\n"); //steering feedback gain
+	ks = kpd_input(0);
+	lcd_clear();
+	lcd_print("Enter value for kdx\r\n");// x direction drive gain
+	kdx = kpd_input(0);
+	lcd_clear();
+	lcd_print("Enter value for kdy\r\n");// y direction drive gain
+	kdy = kpd_input(0);
+	lcd_clear();
+	lcd_print("Enter value for ki\n "); // intergal gain
+	ki  = kpd_input(0);
+	lcd_clear();
+}
+
+void set_steering_pw(){
+    PW_Servo = PW_CTR_SERVO - ks * gx;
+}
+
+void set_driving_pw(){
+    PW_Motor = PW_CENTER_MOTOR + kdy * gy;
+    PW_Motor += kds * abs(gx);
+
+    /* Optional - use integral gain
+    PW_Motor += kdx * abs(gx) + ki * error_sum //ki is the integral gain error_sum += gy + abs(gx)
+    error_sum += gy + abs(gx)
+    */
+}
 
 /*
  * sets proper pulsewidth for the motor
  */
 void start_driving(){
+    PreventExtreme();
     PCA0CP2 = 0xFFFF - PW_Motor;
 }
 
 /*
- * Read and update range values
+ * sets proper pulsewidth for the Servo
  */
-void update_ranger(){
-    if (new_range){ //if 80ms has passed
-        ranger_distance = ReadRanger(); // read the range
-        //start a ping
-        RangerData[0] = 0x51; // write 0x51 to reg 0 of the ranger:
-        i2c_write_data(addr_ranger, 0, RangerData, 1); // write one byte of data to reg 0 at addr
-        new_range = 0; //clear new range flag
-		updateRangerArray(); // append range data to the queue
-    }
-}
-
-/*
- * Select from a predefined list of heading or select manually
- */
-void preselectHeading(){
-    //create an array that represents the 4 bits of heading
-    unsigned int __xdata inputArr[4] = {0,0,0,0}; // using __xdata to store large variables
-    i = 0;
-    printf("Enter 1 to select from the list of headings, or enter 2 to select manually\r\n");
-    input = getchar();
-	input -= 48;
-	printf("\r\n");
-    if (input == 1){ // select from list
-        printf("Enter 1 to to select heading using the keyboard, or enter 2 to select from keypad\r\n" );
-        input = getchar();
-		input -= 48;
-		printf("\r\n");
-        if (input == 1){
-            printf("Enter 1 for 0 deg, 2 for 90 deg, 3 for 180 deg, or 4 for 270 deg \r\n");
-            input = getchar();
-			input -= 48;
-			printf("\r\n");
-        } else if (input == 2){
-            lcd_print("Enter 1 for 0 deg, 2 for 90 deg, 3 for 180 deg, or 4 for 270 deg \r\n");
-            input = read_keypad();
-            input -= 48;
-			printf("\r\n");
-        }
-        switch (input){ // select input based on input values
-            case 1:
-				desired_heading = 0; //0 deg
-                break;
-            case 2:
-                desired_heading = 900; // 90 deg
-                break;
-            case 3:
-                desired_heading = 1800; // 180 deg
-                break;
-            case 4:
-                desired_heading = 2700; // 270 deg
-                break;
-            default:
-                break;
-        }
-		initial_heading = desired_heading;
-    } else if (input == 2){ // select manually
-        printf("Enter 1 to enter the heading using the Keypad, or enter 2 to enter using keyboard\r\n");
-        input = getchar();
-		input -= 48;
-	    printf("\r\n");
-        switch (input){
-            case 1:
-                multipleInput = kpd_input(0);
-                desired_heading = multipleInput;
-                break;
-            case 2:
-                // enter the 4 digits of heading digit by digit
-                while(i < 4){
-                    printf("enter the digit %d \r\n", i);
-                    input = getchar();
-					input -= 48;
-					printf("\r\n");
-                    inputArr[i] = input;
-                    i ++;
-                }
-                // convert to a 4 digit number
-                desired_heading = inputArr[0] * 1000 + inputArr[1]* 100 + inputArr[2]*10 + inputArr[3];
-                break;
-            default:
-                break;
-        }
-		initial_heading = desired_heading;
-    }
+void start_steering(){
+    PreventExtreme();
+    PCA0CP0 = 0xFFFF - PW_Servo;
 }
 
 /*
@@ -266,9 +251,11 @@ void preselectMotorSpd(){
         switch (input){
             case 1:
                 PW_Motor = 2031; // full reverse
+                isReversed = 1;
                 break;
             case 2:
                 PW_Motor = 2300; // 2/3 full reverse
+                isReversed = 1;
                 break;
             case 3:
                 PW_Motor = 3339; // 78% full forward
@@ -310,61 +297,6 @@ void preselectMotorSpd(){
     }
 }
 
-/*
- * execute a hard right
- */
-void turn_right(){
-    if (new_heading){
-        heading = ReadCompass(); // read the current heading
-        new_heading = 0;
-    }
-    // set the desired heading to 90 deg higher than the current heading (right turn)
-    desired_heading = heading + 900;
-    motor_spd -= 200; // decrease motor speed for accurate turning
-    while (getchar_nw() != ' '){ // keep turning until space bar is pressed
-        update_ranger();
-        adjustServo();
-        PW_Motor = motor_spd;
-        start_driving();
-        pw_percentage = (abs(PW_Motor- PW_CENTER)*100)/(PW_MAX- PW_MIN);
-		if (print_flag){ // update LCD
-			printf("%u %u %u\r\n",heading, ranger_distance, PW_Servo);
-			lcd_clear();
-	        lcd_print("direction: %u\nrange: %u\npw perc: %u\nbattery: %u\n",heading,ranger_distance,pw_percentage,read_AD_input(6));
-	        print_flag = 0;
-    	}
-    }
-    motor_spd += 200; // set motor speed back to normal
-    desired_heading = initial_heading;
-}
-
-/*
- * execute a hard left
- */
-void turn_left(){
-	if (new_heading){
-        heading = ReadCompass(); // read the current heading
-        new_heading = 0;
-    }
-    // set the desired heading to 80 deg less than the current heading (leftturn)
-    desired_heading = heading - 800;
-	motor_spd -= 200; // decrease motor speed for accurate turning
-    while (getchar_nw() != ' '){ // keep turning until space bar is pressed
-        update_ranger();
-		adjustServo();
-		PW_Motor = motor_spd;
-		start_driving();
-        pw_percentage = (abs(PW_Motor- PW_CENTER)*100)/(PW_MAX- PW_MIN);
-		if (print_flag){ // update LCD
-			printf("%u %u %u\r\n",heading, ranger_distance, PW_Servo);
-			lcd_clear();
-	        lcd_print("direction: %u\nrange: %u\npw perc: %u\nbattery: %u\n",heading,ranger_distance,pw_percentage,read_AD_input(6));
-	        print_flag = 0;
-    	}
-    }
-	motor_spd += 200; // set motor speed back to normal
-	desired_heading = initial_heading;
-}
 
 /*
  * adjust the servo direction by adjusting the current heading to match the desired heading
@@ -444,33 +376,24 @@ void PCA_ISR ( void ) __interrupt 9 {
     counter_PCA++;
     if (CF){
         CF = 0;// reset flag
-        heading_count++;
         LCD_count ++ ;
-        ranger_count ++;
-        if(heading_count >= 2){ // approx 40 ms
-            new_heading = 1; // flag new heading can be read
-            heading_count = 0; // reset pca count timer
-        }
-        if (ranger_count >= 4){
-            new_range = 1;//flag new range can be read
-            ranger_count = 0;
-        }
+        accels_count ++;
+        reverse_count ++;
+
         if (LCD_count >= 20){// update the display every 400 ms
             print_flag = 1;
             LCD_count = 0;
         }
+        if (accels_count >= 1){
+            accels_flag = 1;
+            accels_count = 0;
+        }
+        if (reverse_count >= 51){
+            reverse_count = 0;
+        }
         PCA0 = PCA_START;
     }
     PCA0CN &= 0xC0; // handle other pca interrupt resources
-}
-
-/*
- * Obtain the compass reading
- */
-unsigned int ReadCompass(){
-    i2c_read_data(addr_compass,2,CompassData,2);   //adress, byte to start, where to story, how many bytes to read
-    heading = ((CompassData[0] << 8) | CompassData[1]); // turn 2 8-bit into one 16 bit
-    return heading;
 }
 
 /*
@@ -488,15 +411,6 @@ void adjust_pw() {
     PCA0CPH0 = (0xFFFF - PW_Servo) >> 8;
 }
 
-/*
- * Function that reads the distance from the ultrasonic ranger
- */
-unsigned int ReadRanger() {
-    unsigned int range = 0;
-    i2c_read_data(addr_ranger, 2, RangerData, 2); // read two bytes, starting at reg 2
-    range = (((unsigned int)RangerData[0] << 8) | RangerData[1]);
-    return range;
-}
 
 /*
  * Ensure both pulse widths are within accepted range
